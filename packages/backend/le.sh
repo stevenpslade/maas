cbececee99120e244eb6f9623854e65127a79ed6
Austin Griffith <you@example.com>
2022-04-30 14:10:59 -0600

    new domain

--- packages/backend/le.sh
+++ packages/backend/le.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-DOMAIN="backend.scholarship.buidlguidl.com"
+DOMAIN="backend.multisig.lol"
 
 sudo certbot certonly --standalone -d $DOMAIN --config-dir ~/.certbot/config --logs-dir ~/.certbot/logs --work-dir ~/.certbot/work
 sudo cp -f ~/.certbot/config/live/$DOMAIN/privkey.pem server.key;sudo chmod 0777 server.key
